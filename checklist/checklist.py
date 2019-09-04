2c188e26c830f2c48561c3cab10aa5df2d4ee456
Joey Gaitan <joeygaitan707@gmail.com>
2019-09-03 19:27:00 -0700

    Did some updates to some of the function names. Made them return something so they wouldn't break. In addition I added in a test function for functionality

--- checklist/checklist.py
+++ checklist/checklist.py
@@ -1,8 +1,38 @@
 print("Hello World")
 
-checklist = list()
-
 def my_fun_function(say_this):
 	print(say_this)
 
+print(my_fun_function('Hello World'))
 
+checklist = list()
+
+#create or add additional information to list
+def create(item):
+	return checklist.append(item)
+	
+
+#Read One Item from an list
+def read(index):
+	return checklist[index]
+
+def update(index,item):
+	checklist[index] = item
+	return
+
+def destroy(index):
+	return checklist.pop(index)
+
+def test():
+	create("purple sox")
+	create("red cloak")
+
+	print(read(0))
+	print(read(1))
+
+	update(0, "purple socks")
+	destroy(1)
+
+	print(read(0))
+
+test()

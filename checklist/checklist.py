6348c7b50a7722949360b6ff31912f4da478506f
Joey Gaitan <joeygaitan707@gmail.com>
2019-09-15 17:11:13 -0700

    added in check feature. Added in uncheck feature.

--- checklist/checklist.py
+++ checklist/checklist.py
@@ -7,19 +7,38 @@ def create(item):
 
 #Read One Item from an list
 def read(index):
-	return checklist[index]
+    print("on line 10")
+    return checklist[index]
+
+def check(index):
+    checkerChecker = checklist[int(index)].split()
+    if checkerChecker[0] == "√":
+       print("This item is already checked")
+    else:
+        checklist[int(index)] = "√" + checklist[int(index)]
+        return True
+
+def uncheck(index):
+    checkerChecker = checklist[int(index)][1:]
+    checklist[int(index)] = checkerChecker
+    
 
 #list all items
 def list_all_items():
-	index = 0
-	for list_item in checklist:
-		print("{} {}".format(index, list_item))
-		index += 1
+    if len(checklist) == 0:
+        return "Please Add some items to your checklist please" 
+    else:
+	    index = 0
+	    for list_item in checklist:
+		    print("{} {}".format(index, list_item))
+		    index += 1
 
 #update element in list
 def update(index,item):
-	checklist[index] = item
-	return
+    if len(checklist) == 0:
+        return "There is nothing to change :/"
+    else:
+	    checklist[index] = item
 
 #remove an element from array
 def destroy(index):
@@ -31,32 +50,40 @@ def user_input(prompt):
     return user_input
 
 def select(function_code):
-    if function_code == "C":
-        input_item = user_input("Input item: ")
-        create(input_item)
-
-    elif function_code == "R":
-        item_index = user_input("Index Number?")
+    if function_code == "C" or function_code=="c":
+        create(user_input("Input item: "))
+        return True
+    elif function_code == "R" or function_code == "r":
+        item_index = user_input("Index Number: ")
         print(checklist[int(item_index)])
-
-    elif function_code == "U":
+        return True
+    elif function_code == "U" or function_code == "u":
         update(user_input("please input an Index you would like to change"),  user_input("please input what you would like to update it with"))
-
+        return update(user_input("please input an Index you would like to change"),  user_input("please input what you would like to update it with"))
     # Print all items
-    elif function_code == "P":
+    elif function_code == "P" or function_code == "p":
         list_all_items()
-
-    elif function_code == "D":
+        return True
+    elif function_code == "D" or function_code == "d":
         destroy(int(user_input("Please input an index for an element you would like to delete")))
-
-    elif function_code == "Q":
+        return True
+    elif function_code == "Q" or function_code == "q":
+        print("Thanks for using this project")
         return False
+    elif function_code == "check" or function_code == "Check":
+        check(user_input("please select which one you want to check: "))
+
+    elif function_code == "uncheck" or function_code == "Uncheck":
+        uncheck(user_input("Please select index of something you want to uncheck: "))
 
     else:
         print("Unknown Option")
 
+
 running = True
 while running:
     selection = user_input(
-        "Press C to add to list, R to Read from list and P to display list, D to delete an elemnt of the and Q to Quit")
-    select(selection)
+        "Press C to add to list, R to Read from list and P to display list,\n D to delete an elemnt of the list.\n To add a checkmark to this list please type Check. To uncheck type uncheck and Q to Quit: ")
+    check_list = select(selection)
+    if check_list == False:
+        running = False
